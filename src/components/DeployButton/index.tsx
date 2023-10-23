74cb41e6bce408304a42212712f4404a0db8e62a
0xernesto <78889960+0xernesto@users.noreply.github.com>
2023-10-23 17:11:56 -0500

    feat: pupulated DeployModal  with ui elements

--- src/components/DeployButton/index.tsx
+++ src/components/DeployButton/index.tsx
@@ -9,7 +9,7 @@ function DeployButton({
 	currentNetwork,
 	openModal,
 }: IDeployButton) {
-	const handleDeploy = async () => {
+	const handleDeployModal = async () => {
 		if (
 			isWalletConnected &&
 			currentNetwork.isSupported &&
@@ -39,7 +39,7 @@ function DeployButton({
 				} as React.CSSProperties
 			}
 			className="offset-border z-10 flex h-10 w-20 shrink-0 items-center justify-center bg-dark-500 px-2 outline-none hover:bg-dark-400 hover:text-primary-100"
-			onClick={handleDeploy}
+			onClick={handleDeployModal}
 		>
 			Deploy
 		</button>
--- src/components/DeployModal/index.tsx
+++ src/components/DeployModal/index.tsx
@@ -1,10 +1,30 @@
 import { Dialog, Transition } from "@headlessui/react";
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import { AiOutlineClose } from "react-icons/ai";
 
 import { IDeployModal } from "../../interfaces/components/deployModal";
 
 function DeployModal({ isOpen, closeModal, chain }: IDeployModal) {
+	const [signed, setSigned] = useState<boolean>(false);
+
+	const getAddress = async () => {
+		// call DeployService.getInstance().getAddress()
+	};
+
+	const handleSignature = async () => {
+		// call DeployService.getInstance().sign()
+		setSigned(true);
+	};
+
+	const handleDeployment = async () => {
+		// call DeployService.getInstance().deploy()
+		setSigned(false);
+	};
+
+	useEffect(() => {
+		getAddress();
+	}, []);
+
 	return (
 		<Transition appear show={isOpen} as={Fragment}>
 			<Dialog
@@ -36,7 +56,9 @@ function DeployModal({ isOpen, closeModal, chain }: IDeployModal) {
 									as="h3"
 									className="flex items-center justify-between"
 								>
-									<span>Deployment</span>
+									<span className="mr-3 font-bold">
+										{chain && chain.name} Deployment
+									</span>
 									<button
 										style={
 											{
@@ -50,11 +72,56 @@ function DeployModal({ isOpen, closeModal, chain }: IDeployModal) {
 										<AiOutlineClose size="20px" />
 									</button>
 								</Dialog.Title>
-								<div className="mt-4 flex flex-col items-center justify-center">
-									<div className="my-2" />
-									<span className="text-xs text-light-400">
-										Deploy to {chain && chain.name}
+								<div className="mt-5 flex flex-col items-start justify-center">
+									<span className="mb-1 underline underline-offset-4">
+										Expected Address
 									</span>
+									<span className="text-light-400">
+										0xdeadbeef
+									</span>
+									{!signed && (
+										<>
+											<span className="mb-1 mt-2 underline underline-offset-4">
+												Disclosure
+											</span>
+											<span className="text-light-400">
+												For security reasons, your
+												signature is required to confirm
+												your intent to deploy your smart
+												contract at the expected address
+												on {chain && chain.name}
+											</span>
+										</>
+									)}
+									<div className="mt-5 self-center">
+										{signed ? (
+											<button
+												style={
+													{
+														"--offset-border-color":
+															"#395754", // dark-200
+													} as React.CSSProperties
+												}
+												className="offset-border z-10 flex h-10 w-20 shrink-0 items-center justify-center bg-dark-500 px-2 outline-none hover:bg-dark-400 hover:text-primary-100"
+												onClick={handleDeployment}
+											>
+												Deploy
+											</button>
+										) : (
+											<button
+												style={
+													{
+														"--offset-border-color":
+															"#395754", // dark-200
+													} as React.CSSProperties
+												}
+												className="offset-border z-10 flex h-10 w-20 shrink-0 items-center justify-center bg-dark-500 px-2 outline-none hover:bg-dark-400 hover:text-primary-100"
+												onClick={handleSignature}
+											>
+												Sign
+											</button>
+										)}
+									</div>
 								</div>
 							</Dialog.Panel>
 						</Transition.Child>
