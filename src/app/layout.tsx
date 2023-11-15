d919dc34a43a5328636e7cf5156157ef46a4cc5b
0xernesto <78889960+0xernesto@users.noreply.github.com>
2023-11-15 01:44:00 -0600

    refactor: upgrade Next.js and migrate metadata to RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,85 @@
 import "../styles/globals.css";
 
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import React from "react";
 
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import { ReduxProvider } from "../state/reduxProvider";
 
+export const viewport: Viewport = {
+	themeColor: "#000000", // dark-600
+};
+
 export const metadata: Metadata = {
+	metadataBase: new URL("https://xsafe.chainrule.io"),
 	title: "xSafe",
 	description:
 		"Redefining multi-chain deployment: same address, no nonces, no salts.",
+	keywords: [
+		"Open-Source",
+		"Code",
+		"Math",
+		"Chain Rule",
+		"xSafe",
+		"DeFi",
+		"Crypto",
+		"Smart Contracts",
+		"EVM",
+		"Deploy",
+		"Cross-Chain",
+		"Multi-Chain",
+		"Ethereum",
+		"Optimism",
+		"Polygon",
+		"BNB Smart Chain",
+		"Gnosis",
+		"Arbitrum One",
+		"Base",
+		"Avalanche C-Chain",
+		"Celo",
+		"Fantom",
+		"Sepolia",
+		"Mumbai",
+	],
+	icons: {
+		icon: [
+			{
+				rel: "icon",
+				type: "image/png",
+				sizes: "32x32",
+				url: "/favicon-32x32.png",
+			},
+			{
+				rel: "icon",
+				type: "image/png",
+				sizes: "16x16",
+				url: "/favicon-16x16.png",
+			},
+			{
+				rel: "apple-touch-icon",
+				sizes: "180x180",
+				url: "/apple-touch-icon.png",
+			},
+			{
+				rel: "icon",
+				type: "image/png",
+				sizes: "192x192",
+				url: "/android-chrome-192x192.png",
+			},
+			{
+				rel: "icon",
+				type: "image/png",
+				sizes: "512x512",
+				url: "/android-chrome-512x512.png",
+			},
+			{
+				rel: "mask-icon",
+				url: "/safari-pinned-tab.svg",
+				color: "#395754", // dark-200
+			},
+		],
+	},
 	openGraph: {
 		title: "xSafe",
 		description:
@@ -19,7 +88,7 @@ export const metadata: Metadata = {
 		type: "website",
 		images: [
 			{
-				url: "https://xsafe.chainrule.io/card-image.png?123",
+				url: "/card-image.png",
 			},
 		],
 	},
@@ -30,10 +99,10 @@ export const metadata: Metadata = {
 		card: "summary_large_image",
 		images: [
 			{
-				url: "https://xsafe.chainrule.io/card-image.png?123",
+				url: "/card-image.png",
 			},
 		],
-		site: "https://xsafe.chainrule.io/",
+		site: "@chainrule_io",
 	},
 };
 
